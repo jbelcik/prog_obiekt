3dcb7562b4d4c0fa3f3026de48df3a93b4ece8f1
Your Name <belcik.jakub@gmail.com>
2011-12-13 22:43:46 +0100

    groch

--- Student.java
+++ Student.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 
-class Student
+class Student //implements Comparable<Student>
 {
   int nr;
   String imie;
@@ -35,7 +35,29 @@ class Student
   {
     return nr + " " + imie + "\n" + ocenyToString();
   }
+
+  /*public int compareTo(Student s)
+  {
+    return this.imie.compareTo(s.imie);
+  }*/
 }
+
+
+/*class Porownaj implements Comparator<Student>
+{    
+  public int compare(Student s1, Student s2)
+  {
+    int i,
+        max1 = 0,
+        max2 = 0;
+
+    for (i = 0; i < s1.oceny.size(); i++) if (s1.oceny.get(i) > max1) max1 = s1.oceny.get(i);
+    for (i = 0; i < s2.oceny.size(); i++) if (s2.oceny.get(i) > max2) max2 = s2.oceny.get(i);
+
+    if (max1 >= max2) return -1;
+    else return 1;
+  }
+}*/
 
 
 class WykazS
@@ -70,6 +92,16 @@ class WykazS
       System.out.println(s.toString()); 
     }
   }
+
+  /*public void sortujA()
+  {
+    Collections.sort(wykaz);
+  }
+
+  public void sortujM()
+  {
+    Collections.sort(wykaz, new Porownaj());
+  }*/
 }
 
 
@@ -102,6 +134,6 @@ class TestWykazS
     ws.wstawOcene(189557,5);
 
     ws.wypisz();
-    ws.wypisz(199200);
+    //ws.wypisz(199200);
   }
 }
